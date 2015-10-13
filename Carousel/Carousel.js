0b7fcfc891091804eb6966bb73c5a302ac6fd2b7
VladSamodin <samodin.vladislav@gmail.com>
2015-10-13 12:48:15 +0200

    refactoring carousel constructor

--- Carousel/Carousel.js
+++ Carousel/Carousel.js
@@ -27,13 +27,16 @@ var getItmes = function() {
         ];
 }
 
+var bindMethod = function(o, f) {
+    return function() { return f.apply(o, arguments);};
+};
+
 var Carousel = function(items, viewport, itemWidth, container) {
     if (items.length === 0 || !container || !items)
         throw new ReferenceError();
     if (itemWidth <= 0)
         throw new RangeError();
 
-    var self = this;
     var itemsPerPage = 3;
     var arrowWidth = 45;
     viewport = viewport || "900";
@@ -47,113 +50,130 @@ var Carousel = function(items, viewport, itemWidth, container) {
     var marginWidth = ((viewport / 3) - itemWidth - 2 * borederWidth - 2 * paddingWidth) / 2;
     var additionalWidth = (borederWidth + paddingWidth + marginWidth) * 2;
     
-    this.data = items;
     this.selectedItem = 0;
     this.currentShiftView = 0;
     this.firstVisible = 0;
     this.lastVisible =  items.length > itemsPerPage ? itemsPerPage - 1 : items.length - 1;
 
-    var createItem = function(img_src, description) {
-        var item, img, p, margin;
-        item = document.createElement('li');
-        item.classList.add('carousel-item');
-        item.style.marginLeft = marginWidth + "px";
-        item.style.marginRight = marginWidth + "px";
-        img = document.createElement('img');
-        img.setAttribute('src', img_src);
-        img.style.width = itemWidth;
-        p = document.createElement('p');
-        p.innerHTML = description;
-        p.style.width = itemWidth;
-        item.appendChild(img);
-        item.appendChild(p);
-        return item;
-    };
+    this.data = items;
+    this.itemWidth = itemWidth;
+    this.container = container;
+    this.viewport = viewport;
+    this.itemsPerPage = itemsPerPage;
+    this.arrowWidth = arrowWidth;
+    this.borederWidth = borederWidth;
+    this.paddingWidth = paddingWidth;
+    this.marginWidth = marginWidth;
+    this.additionalWidth = additionalWidth;
+}
 
-    var createItems = function (dataArray) {
-        var i, items = [];
-        for (i = 0; i < dataArray.length; i++) {
-            items.push(createItem(dataArray[i].img_src, dataArray[i].description));
-        }
-        items[0].classList.add("carousel-item-selected");
-        return items;
-    };
-
-    var createItemContainer = function(items) {
-        var itemContainer = document.createElement('ul');
-        itemContainer.classList.add('carousel-item-container');
-        itemContainer.style.width = (itemWidth + additionalWidth) * items.length;
-        for (i = 0; i < items.length; i++) {
-            itemContainer.appendChild(items[i]);
-        }
-        return itemContainer;
-    };
-
-    var createView = function(itemContainer) {
-        var view = document.createElement("div");
-        view.classList.add("carousel-view");
-        view.appendChild(itemContainer);
-        return view;
+Carousel.prototype.next = function() {
+    if (this.selectedItem >= this.items.length - 1)
+        return;
+    this.items[this.selectedItem].classList.remove("carousel-item-selected");
+    this.selectedItem++;
+    this.items[this.selectedItem].classList.add("carousel-item-selected");
+    if (this.selectedItem > this.lastVisible) {
+        this.currentShiftView += -(this.itemWidth + this.additionalWidth);
+        this.itemContainer.style.transform = "translateX(" + this.currentShiftView + "px)";
+        this.lastVisible++;
+        this.firstVisible++;
     }
+};
 
-    var createPrevButton = function() {
-        var prevButton = document.createElement('div');
-        prevButton.innerHTML = "&#9668;";
-        prevButton.classList.add("carousel-arrow");
-        prevButton.classList.add("carousel-arrow-left");
-        prevButton.addEventListener("click", function() {
-            if (self.selectedItem === 0)
-                return;
-            self.items[self.selectedItem].classList.remove("carousel-item-selected");
-            self.selectedItem--;
-            self.items[self.selectedItem].classList.add("carousel-item-selected");
-            if (self.selectedItem < self.firstVisible){
-                self.currentShiftView += (itemWidth + additionalWidth);
-                self.itemContainer.style.transform = "translateX(" + self.currentShiftView + "px)";
-                self.lastVisible--;
-                self.firstVisible--;
-            }
-        });
-        return prevButton;
+Carousel.prototype.prev = function() {
+    if (this.selectedItem === 0)
+        return;
+    this.items[this.selectedItem].classList.remove("carousel-item-selected");
+    this.selectedItem--;
+    this.items[this.selectedItem].classList.add("carousel-item-selected");
+    if (this.selectedItem < this.firstVisible) {
+        this.currentShiftView += (this.itemWidth + this.additionalWidth);
+        this.itemContainer.style.transform = "translateX(" + this.currentShiftView + "px)";
+        this.lastVisible--;
+        this.firstVisible--;
     }
+};
 
-    var createNextButton = function() {
-        var nextButton = document.createElement('div');
-        nextButton.innerHTML = "&#9658;";   
-        nextButton.classList.add("carousel-arrow");
-        nextButton.classList.add("carousel-arrow-right");
-        nextButton.addEventListener("click", function() {
-            if (self.selectedItem >= self.items.length - 1)
-                return;
-            self.items[self.selectedItem].classList.remove("carousel-item-selected");
-            self.selectedItem++;
-            self.items[self.selectedItem].classList.add("carousel-item-selected");
-            if (self.selectedItem > self.lastVisible){
-                self.currentShiftView += -(itemWidth + additionalWidth);
-                self.itemContainer.style.transform = "translateX(" + self.currentShiftView + "px)";
-                self.lastVisible++;
-                self.firstVisible++;
-            }
-        });
-        return nextButton;
+Carousel.prototype._createItem = function(img_src, description) {
+    var item, img, p;
+    item = document.createElement('li');
+    item.classList.add('carousel-item');
+    item.style.marginLeft = this.marginWidth + "px";
+    item.style.marginRight = this.marginWidth + "px";
+    img = document.createElement('img');
+    img.setAttribute('src', img_src);
+    img.style.width = this.itemWidth;
+    p = document.createElement('p');
+    p.innerHTML = description;
+    p.style.width = this.itemWidth;
+    item.appendChild(img);
+    item.appendChild(p);
+    return item;
+};
+
+Carousel.prototype._createItems = function (dataArray) {
+    var i, items = [];
+    for (i = 0; i < dataArray.length; i++) {
+        items.push(this._createItem(dataArray[i].img_src, dataArray[i].description));
     }
+    items[0].classList.add("carousel-item-selected");
+    return items;
+};
 
-    this.items = createItems(items);
-    this.itemContainer = createItemContainer(this.items);
+Carousel.prototype._createItemContainer = function(items) {
+    var itemContainer = document.createElement('ul');
+    itemContainer.classList.add('carousel-item-container');
+    itemContainer.style.width = (this.itemWidth + this.additionalWidth) * items.length;
+    for (i = 0; i < items.length; i++) {
+        itemContainer.appendChild(items[i]);
+    }
+    return itemContainer;
+};
+
+Carousel.prototype._createView = function(itemContainer) {
+    var view = document.createElement("div");
+    view.classList.add("carousel-view");
+    view.appendChild(itemContainer);
+    return view;
+};
+
+Carousel.prototype._createPrevButton = function() {
+    var prevButton = document.createElement('div');
+    prevButton.innerHTML = "&#9668;";
+    prevButton.classList.add("carousel-arrow");
+    prevButton.classList.add("carousel-arrow-left");
+    prevButton.addEventListener("click", bindMethod(this, Carousel.prototype.prev));
+    return prevButton;
+};
+
+Carousel.prototype._createNextButton = function() {
+    var nextButton = document.createElement('div');
+    nextButton.innerHTML = "&#9658;";   
+    nextButton.classList.add("carousel-arrow");
+    nextButton.classList.add("carousel-arrow-right");
+    nextButton.addEventListener("click", bindMethod(this, Carousel.prototype.next));
+    return nextButton;
+};
+
+Carousel.prototype.build = function() {
+    this.items = this._createItems(this.data);
+    this.itemContainer = this._createItemContainer(this.items);
 
     var carouselContainer = document.createElement("div");
     carouselContainer.classList.add("carousel-container")
-    //carouselContainer.style.width = (itemWidth + additionalWidth) * itemsPerPage;
-    carouselContainer.style.width = viewport;
+    carouselContainer.style.width = this.viewport;
 
-    carouselContainer.appendChild(createView(this.itemContainer));
-    carouselContainer.appendChild(createPrevButton());
-    carouselContainer.appendChild(createNextButton());
+    carouselContainer.appendChild(this._createView(this.itemContainer));
+    carouselContainer.appendChild(this._createPrevButton());
+    carouselContainer.appendChild(this._createNextButton());
 
-    container.appendChild(carouselContainer);
-}
+    this.container.appendChild(carouselContainer);
+};
+
 
 
 window.addEventListener("load", function() {
-    new Carousel(getItmes(), 750, 200, document.getElementById('container'));
+    var carusel = new Carousel(getItmes(), 1200, 300, document.getElementById('container'));
+    carusel.build();
 });
